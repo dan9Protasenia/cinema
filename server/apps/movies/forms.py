395542852143c47d84b5c2a2661e6c823d495a2f
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-08-18 22:33:04 +0300

    feature/movie

--- server/apps/movies/forms.py
+++ server/apps/movies/forms.py
@@ -4,6 +4,8 @@ from .models import Movie
 
 
 class MovieForm(forms.ModelForm):
+    genres = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Movie
         fields = '__all__'
--- server/apps/movies/models.py
+++ server/apps/movies/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.CharField()
     release_date = models.DateField()
-    poster = models.ImageField(upload_to='movie_posters/', default='default_poster.jpg')
-    genres = models.ManyToManyField(Genre)
+    poster = models.ImageField(upload_to='movie_posters/', default='movie_posters/default_poster.jpg')
+    genres = models.CharField()
+    director = models.CharField(max_length=200)
+    cast = models.CharField(max_length=200)
+    production = models.CharField()
+    time = models.CharField()
 
     def __str__(self):
         return self.title
